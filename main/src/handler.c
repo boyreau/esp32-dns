b6a8ef927d36e9307a3370060ac2d67734117083
Arthur <bnzlvosnb@mozmail.com>
2025-03-02 13:19:03 +0100

    correct DNS queries answering. IP addresses are now retrieved from the trie. Debug logs cleanup.

--- main/src/handler.c
+++ main/src/handler.c
@@ -6,18 +6,18 @@
 /*   By: aboyreau <bnzlvosnb@mozmail.com>                     +**+ -- ##+     */
 /*                                                            # *   *. #*     */
 /*   Created: 2025/02/19 12:37:58 by aboyreau          **+*+  * -_._-   #+    */
-/*   Updated: 2025/03/01 19:48:14 by aboyreau          +#-.-*  +         *    */
+/*   Updated: 2025/03/02 13:12:34 by aboyreau          +#-.-*  +         *    */
 /*                                                     *-.. *   ++       #    */
 /* ************************************************************************** */
 
-#include "cc.h"
 #include "dns.h"
-#include "esp_log.h"
-#include "lwip/sockets.h"
 #include "pstring.h"
 #include "trie.h"
 
+#include <cc.h>
+#include <esp_log.h>
 #include <lwip/netdb.h>
+#include <lwip/sockets.h>
 #include <string.h>
 #include <sys/param.h>
 
@@ -28,7 +28,6 @@
 static int
 receive_packet(int sock, struct sockaddr_storage *source_addr, void *packet)
 {
-	ESP_LOGI(TAG, "Dest: %p", (void *) packet);
 	socklen_t socklen = sizeof(*source_addr);
 
 	int len = recvfrom(
@@ -59,6 +58,7 @@ static int receive_and_respond(int sock, struct sockaddr_storage *source_addr)
 	struct dns_header *header = &packet.header;
 
 	static struct trie_s head = {0};
+	trie_add(&head, "\nsciodog.fr", 603187885);
 
 	messagelen = receive_packet(sock, source_addr, &packet);
 	if (messagelen < 0)
@@ -75,46 +75,54 @@ static int receive_and_respond(int sock, struct sockaddr_storage *source_addr)
 	header->ra		= 0;
 	header->rcode	= 0;
 	header->ancount = 0;
+	header->arcount = 0;
 
-	uint16_t index = DNS_HEADER_BYTE_SIZE;
+	char	*rr		  = malloc(DNS_PACKET_SIZE_MAX);
+	uint16_t rr_index = 0;
+	uint16_t index	  = DNS_HEADER_BYTE_SIZE;
 	for (uint16_t i = 0; i < header->qdcount; i++)
 	{
-		char *question_address = (char *) &packet + index;
-		ESP_LOGI(TAG, "Reading from: %p", (void *) question_address);
+		char			   *question_address = (char *) &packet + index;
 		struct dns_question q =
 			dns_parse_question((void *) question_address, messagelen - index);
 		dns_log_question(q);
 
-		int ip = (int) trie_get(&head, q.qname);
+		uint32_t ip = (uint32_t) trie_get(&head, q.qname);
 		if (ip == 0)
 		{
 			// TODO Recursive query
+			free(q.qname);
+			continue;
 		}
 		struct dns_rr an = {
 			.type	  = htons(1),
 			.class	  = htons(1),
 			.ttl	  = htonl(0),
 			.rdlength = htons(4),
-			.rddata	  = (uint32_t) htonl(ip)
+			.rddata	  = htonl(ip)
 		};
-		// Copy the DNS labels
-		memcpy(packet.raw + index, question_address, pstr8_len(q.qname) + 2);
+		// Copy the DNS labels (+2: leading size byte and trailing empty byte)
+		memcpy(rr + rr_index, question_address, pstr8_len(q.qname) + 2);
+		rr_index += pstr8_len(q.qname) + 2;
 		index += pstr8_len(q.qname) + 2;
-		memcpy(packet.raw + index, &an, sizeof(an));
-		index += sizeof(uint32_t);
+
+		// Copy the resource record informations
+		memcpy(rr + rr_index, &an, sizeof(an));
+		rr_index += sizeof(an);
+
+		index += 4; // qtype + qclass size
+
+		free(q.qname);
 		header->ancount++;
 	}
 
-	// build the answer
-
 	dns_ntoh_header(header);
+	memcpy(packet.raw + index, rr, rr_index);
 
 	sendto(
 		sock,
 		&packet,
-		512,
-		// DNS_HEADER_BYTE_SIZE + pstr8_len(dns) + 5 + pstr8_len(dns) + 1 +
-		// sizeof(struct dns_rr),
+		index + rr_index,
 		0,
 		(struct sockaddr *) source_addr,
 		sizeof(*source_addr)
